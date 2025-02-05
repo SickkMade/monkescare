c1887f33523623ec7c3aaf5f0150984e88b59ec7
sickk <jaakmyr@gmail.com>
2025-02-05 00:21:37 -0800

    added toolbar and restart

--- src/Components/Toolbar.jsx
+++ src/Components/Toolbar.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { FaWrench } from "react-icons/fa";
+import { FaMountain } from "react-icons/fa";
+import { FaQuoteLeft } from "react-icons/fa";
+import { FaClock } from "react-icons/fa";
+import { FaFont } from "react-icons/fa";
+import { FaHashtag } from "react-icons/fa";
+import { FaAt } from "react-icons/fa";
 
 function Toolbar() {
   return (
-    <div>Toolbar</div>
+    <>
+    <div className="app--hoverable"><FaAt />punctuation</div>
+    <div className="app--hoverable"><FaHashtag/>numbers</div>
+    <div className="app--hoverable toolbar--line-left"><FaClock/>time</div>
+    <div className="app--hoverable"><FaFont/>words</div>
+    <div className="app--hoverable"><FaQuoteLeft/>quote</div>
+    <div className="app--hoverable"><FaMountain/>zen</div>
+    <div className="app--hoverable"><FaWrench/>custom</div>
+    <div className="app--hoverable toolbar--line-left">change</div>
+    </>
   )
 }
 
